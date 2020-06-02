95df8534e3aea3007dc9cdfdf655975c8d1f9a7c
drake.aren <drake.aren@protonmail.com>
2020-06-02 13:20:50 +0600

    Add lein drip

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,12 @@
-{:user {:signing {:gpg-key "F4EA56454DAB2160651DAF56652C55B7AEB9EEF1"}
- :plugins
- [[refactor-nrepl "2.5.0-SNAPSHOT"]
-  [cider/cider-nrepl "0.23.0-SNAPSHOT"]
-  [lein-cljfmt "0.6.4"]
-  [venantius/ultra "0.6.0"]]
- :dependencies
- [[jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
-  [cljfmt "0.5.1"]
-  [alembic "0.3.2"]]}}
+{:user
+ {:signing {:gpg-key "F4EA56454DAB2160651DAF56652C55B7AEB9EEF1"}
+  :plugins
+   [[refactor-nrepl "2.5.0-SNAPSHOT"]
+    [cider/cider-nrepl "0.23.0-SNAPSHOT"]
+    [lein-drip "0.1.1-SNAPSHOT"]
+    [lein-cljfmt "0.6.4"]
+    [venantius/ultra "0.6.0"]]
+  :dependencies
+  [[jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
+   [cljfmt "0.5.1"]
+   [alembic "0.3.2"]]}}
