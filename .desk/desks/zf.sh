f8c097aa1182b844e6cbedbee50c582ffaeb197d
drake.aren <drake.aren@protonmail.com>
2020-06-02 13:20:40 +0600

    Add mobile snippets for zf desk

--- .desk/desks/zf.sh
+++ .desk/desks/zf.sh
@@ -18,4 +18,24 @@ setup_local_backend() {
   cat ./.env.local > ./.env
 }
 
+mobile_android_real() {
+  tmux \
+    new-session "yarn start; read" \; \
+    new-window "clj -R:repl build.clj figwheel -p android -a real; read" \;
+}
+
+mobile_android_avd() {
+  tmux \
+    new-session "yarn start; read" \; \
+    new-window "clj -R:repl build.clj figwheel -p android -a real; read" \; \
+    new-window "emulator -avd Pixel_3a_XL_API_29; read" \;
+}
+
+mobile_android_install() {
+  npx react-native run-android --variant=stageDebug --appId io.urbest.stage
+}
+
+alias mreal='mobile_android_real'
+alias mavd='mobile_android_avd'
+alias minstall='mobile_android_install'
 alias bk_local='setup_local_backend'
