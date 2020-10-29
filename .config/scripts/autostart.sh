43fbbc98c0cb369fb2707268b0585f697fded3c6
drake.aren <drake.aren@protonmail.com>
2020-10-29 23:12:08 +0600

    Update autostart sh

--- .config/scripts/autostart.sh
+++ .config/scripts/autostart.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 setxkbmap -option grp:caps_toggle us,ru
-polybar top &
-polybar bottom &
 powerline-daemon -q &
 compton --config ~/.config/compton/compton.conf -b &
 guake &
 blueman-applet &
-~/.fehbg &
 nm-applet &
+tmux new -d -s general &
+xrandr --output HDMI-A-0 --right-of eDP &
+~/.config/polybar/scripts/launch.sh &
+## Wallpapers
+## Random image from unsplash
+# ~/.config/scripts/unsplash_wallpaper.sh &
+## Previous image
+# ~/.fehbg &
+## Random from folder
+~/.config/scripts/random_wallpaper.sh ~/wallpapers
