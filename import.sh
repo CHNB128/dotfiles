da8439c902a708f0627c95339d72d047e6e8b1c7
Drake Aren <drake.aren@pm.me>
2021-03-21 14:13:51 +0600

    Update import

--- import.sh
+++ import.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-ROOT=$(dirname "$0")
-
-ln -sf $ROOT/resources/.config ~/
-ln -sf $ROOT/resources/.zshrc ~/
-ln -sf $ROOT/resources/.tmux.conf ~/
+find ./resources -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -I % ln -sf $PWD/% ~/
+find ./resources -mindepth 1 -maxdepth 1 -type f -print0 | xargs -0 -I % ln -sf $PWD/% ~/
