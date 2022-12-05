71329dc4f4c08bea55847e9c9b80a4414640e97b
Drake Nelson <drake.aren@pm.me>
2022-12-06 00:17:12 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #/bin/sh
 
 # System
-yay -S zsh xorg-server i3-gaps ttf-firacode-nerd 
+yay -S zsh xorg-server xclip libnotify i3-gaps alsa-utils alsa-lib pulseaudio
+# Shell
+yay -S zsh exa bat starship
+# Fonts 
+yay -S ttf-firacode-nerd
 # DE
-yay -S polybar rofi starship feh flameshot starship bat exa
+yay -S polybar rofi starship feh flameshot 
+# Admin
+yay -S htop docker docker-compose
 # Development
 yay -S python3 neovim tmux lazygit
 # Applications
