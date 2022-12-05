3e1df9ed2fa4336b53d75f7321478ba8e5f8f7f1
Drake Nelson <drake.aren@pm.me>
2022-12-05 18:35:21 +0600

    Add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+# System
+yay -S zsh xorg-server i3-gaps ttf-firacode-nerd 
+# DE
+yay -S polybar rofi starship feh flameshot starship bat exa
+# Development
+yay -S python3 neovim tmux lazygit
+# Applications
+yay -S firefox  
