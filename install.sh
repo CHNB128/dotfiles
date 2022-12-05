be0c2c22f77252893b9622a275cad9548fec812e
Drake Nelson <drake.aren@pm.me>
2022-12-06 00:38:49 +0600

    Update install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # System
 yay -S zsh xorg-server xclip libnotify i3-gaps alsa-utils alsa-lib pulseaudio
 # Shell
-yay -S zsh exa bat starship
+yay -S zsh exa bat starship fzf
 # Fonts 
 yay -S ttf-firacode-nerd
 # DE
