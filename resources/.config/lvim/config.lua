da766158b43ee2cb227110366eda2f7c007fca9e
Velichko Ilya <drake.aren@pm.me>
2024-09-20 12:05:25 +0500

    Update lua

--- resources/.config/lvim/config.lua
+++ resources/.config/lvim/config.lua
@@ -15,6 +15,7 @@ vim.opt.relativenumber = true -- set relative numbered lines
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
+-- lvim.colorscheme = "kanagawa-lotus"
 lvim.colorscheme = "lunar"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -63,6 +64,23 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 --   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
 --   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
 -- }
+-- Spectre
+lvim.builtin.which_key.mappings["R"] = {
+  name = "Spectre",
+  r = { '<cmd>:lua require("spectre").open()<cr>', "Open" },
+  R = { '<cmd>:lua require("spectre").open_visual({select_word=true})<cr>', "Open visual (current word)" },
+  v = { '<cmd>:lua require("spectre").open_visual()<cr>', "Open visual" },
+  f = { '<cmd>:lua require("spectre").open_file_search()<cr>', "File search" },
+  F = { '<cmd>:lua require("spectre").open_file_search({select_word=true})<cr>', "File search" },
+}
+-- Hop
+lvim.builtin.which_key.mappings["j"] = {
+  name = "Hop",
+  w = { "<cmd>:HopWord<cr>", "Word" },
+  a = { "<cmd>:HopAnywhere<cr>", "Anywhere" },
+  l = { "<cmd>:HopLine<cr>", "Line" },
+  c = { "<cmd>:HopChar2<cr>", "Two chars" },
+}
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -70,7 +88,7 @@ lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -173,9 +191,9 @@ lvim.plugins = {
   {
     'sbdchd/neoformat',
   },
-  {
-    'github/copilot.vim'
-  },
+  -- {
+  --   'github/copilot.vim'
+  -- },
   -- {
   --   'wakatime/vim-wakatime'
   -- },
@@ -188,20 +206,13 @@ lvim.plugins = {
       vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
     end,
     init = function()
-      lvim.builtin.which_key.mappings["j"] = {
-        name = "Hop",
-        w = { "<cmd>:HopWord<cr>", "Word" },
-        a = { "<cmd>:HopAnywhere<cr>", "Anywhere" },
-        l = { "<cmd>:HopLine<cr>", "Line" },
-        c = { "<cmd>:HopChar2<cr>", "Two chars" },
-      }
     end
   },
   {
     'EdenEast/nightfox.nvim',
   },
   {
-     "rebelot/kanagawa.nvim"
+    "rebelot/kanagawa.nvim"
   },
   {
     "windwp/nvim-ts-autotag",
@@ -223,20 +234,12 @@ lvim.plugins = {
     config = function()
       require("spectre").setup()
     end,
-    init = function()
+    -- init = function()
       -- map('n', '<leader>R', ':lua require("spectre").open()<CR>', {})
       -- map('n', '<leader>rw', ':lua require("spectre").open_visual({select_word=true})<CR>', {})
       -- map('v', '<leader>rv', ':lua require("spectre").open_visual()<CR>', {})
       -- map('n', '<leader>rp', ':lua require("spectre").open_file_search()<CR>', {})
-      lvim.builtin.which_key.mappings["R"] = {
-        name = "Spectre",
-        r = { '<cmd>:lua require("spectre").open()<cr>', "Open" },
-        R = { '<cmd>:lua require("spectre").open_visual({select_word=true})<cr>', "Open visual (current word)" },
-        v = { '<cmd>:lua require("spectre").open_visual()<cr>', "Open visual" },
-        f = { '<cmd>:lua require("spectre").open_file_search()<cr>', "File search" },
-        F = { '<cmd>:lua require("spectre").open_file_search({select_word=true})<cr>', "File search" },
-      }
-    end
+    -- end
   },
 }
 
